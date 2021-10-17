ba5ffdbc697f02995aa4508620e7939073ff761b
Oğuzhan <56599159+basoguz52@users.noreply.github.com>
2021-10-17 12:09:10 +0300

    Add files via upload

---
+++ Asal_mı.rb
@@ -0,0 +1,31 @@
+def asalmı (sayı)
+    if sayı > 1
+        asal = (2...sayı).to_a
+        t = 0
+
+        asal.each do |e|
+
+            t += e if sayı %e == 0
+        
+        end
+        
+        if t == 0
+            puts "Asaldır"
+        
+        else
+            puts "Asal Değildir"
+        end
+    else
+        puts"Asal Değildir"
+    
+    end
+
+end
+
+puts "Bir sayı giriniz."
+x = gets.chomp.to_i
+asalmı x
+sleep (3)
+
+system("start C:/Users/Oğuzhan/Ruby/Asal_mı.rb")
+system("start E:\Asal_mı.rb")
